f57bfbf78efc1a5d734b31608d6e6119e9d14dd4
mynalmeisbalazs <61650542+mynalmeisbalazs@users.noreply.github.com>
2021-09-30 07:48:30 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
 int main()
 {
-    int v[100][100],n,i,j;
-    cout<<"n=";
-    cin>>n;
-    for(i=0;i<n;i++){
-        for( j=0;j<n;j++){
-            cout<<"v["<<i<<"]["<<j<<"]=";
-            cin>v[i][j];
-
+int v[100][100],n,S=0,S2=0;
+ifstream in("input.txt");
+cin>>n;
+    for (int i=0;i<n;i++){
+    for (int j=0;j<n;j++){
+            in>>v[i][j];
+    }
+    }
+    for (int i=0;i<n;i++){
+    for (int j=0;j<n;j++){
+            if(i<j&&i+j<n-1){
+    S=S+v[i][j];
+            }
+        if(i>j&&i+j>n-1){
+    S2=S2+v[i][j];
         }
     }
-    for (i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            cout<< v[i][j]<<" ";
-        }
     }
-    cout << endl;
+    if(S>S2)
+    {
+    cout<<"a deli a nagyobb";
+    }else{
+    cout<<"az eszaki nagzobb";
+    }
     return 0;
+
 }
