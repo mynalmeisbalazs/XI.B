7dd1ce2a13db6e26f42dcfc076c84cda0b1c870d
mynalmeisbalazs <61650542+mynalmeisbalazs@users.noreply.github.com>
2021-10-07 07:35:12 +0300

    Update Kétdimenziós tömb 1 3feladat.cpp

--- Kétdimenziós tömb 1 3feladat.cpp
+++ Kétdimenziós tömb 1 3feladat.cpp
@@ -3,39 +3,36 @@
 
 using namespace std;
 
-int main()
-{
-int v[100][100],i,j,n,S1=0,S2=0,S3=0,S4=0;
-cin>>n;
+int main() {
+    int v[100][100],i,j,n,S1=0,S2=0,S3=0,S4=0;
+    cin>>n;
 
-ifstream in("input.txt");
-ofstream out("output.txt");
+    ifstream in("input.txt");
 
-for(i=0;i<n;i++){
-for(j=0;j<n;j++){
-    in>>v[i][j];
-}
-}
-while (in>>v[i][j]){
-  for(i=0;i<n;i++){
-  for(j=0;j<n;j++){
-    if(i<j&&i+j<n-1){
-    S1=S1+v[i][j];
+    for(i=0; i<n; i++) {
+        for(j=0; j<n; j++) {
+            in>>v[i][j];
+        }
     }
-    if(i<j&&i+j>n-1){
-    S2=S2+v[i][j];
+    while (in>>v[i][j]) {
+        for(i=0; i<n; i++) {
+            for(j=0; j<n; j++) {
+                if(i<j&&i+j<n-1) {
+                    S1=S1+v[i][j];
+                }
+                if(i<j&&i+j>n-1) {
+                    S2=S2+v[i][j];
+                }
+                if(i>j&&i+j<n-1) {
+                    S3=S3+v[i][j];
+                }
+                if(i>j&&i+j>n-1) {
+                    S4=S4+v[i][j];
+                }
+            }
+        }
     }
-    if(i>j&&i+j<n-1){
-    S3=S3+v[i][j];
-    }
-    if(i>j&&i+j>n-1){
-    S4=S4+v[i][j];
-    }
-  }
-  }
-}
-  out<<S1<<S2<<S3<<S4<<endl;
-  in.close();
-  out.close();
-  return 0;
+    cout << S1 << endl << S2 << endl << S3 << endl << S4 << endl;
+    in.close();
+    return 0;
 }
